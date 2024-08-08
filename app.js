0273f3de49507a1a89736df7b59222343d5469cf
abhinav455 <bhattacharjeeabhinav@gmail.com>
2024-08-09 01:31:36 +0530

    updated

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ const app = express();
 const path = require("path");
 
 const port = process.env.PORT || 8080;
+const host = process.env.HOST || '0.0.0.0';
 
-app.use(express.static(path.join(__dirname, "/build")));
+app.use(express.static(path.join(__dirname, "/build"))); //express should understand the path //middleware to serve other static files
 
 app.get("*", (req,res)=> {
-    res.sendFile(path.join(__dirname, "/build/index.html"));
+    res.sendFile(path.join(__dirname + "/build/index.html"));
 });
+
+app.listen(port, host, (_)=> console.log(`react app is listening on port ${port}`));
+
+
+
+
--- deploy.sh
+++ deploy.sh
@@ -4,32 +4,32 @@
 
 echo "Deploying eShop/server to google cloud \n"
 
-# #FN Returns a list of all gcloud deployment versions
-# gcloud_list() {
-#     local gcloud="$(gcloud -q app versions list)"
-#     local array=()
-#     for word in $gcloud
-#     do
-#      array+=($word)
-#     done
-#     echo "${array[@]}" #return 
-# }
+#FN Returns a list of all gcloud deployment versions
+gcloud_list() {
+    local gcloud="$(gcloud -q app versions list)"
+    local array=()
+    for word in $gcloud
+    do
+     array+=($word)
+    done
+    echo "${array[@]}" #return 
+}
 
-# # FN Deletes unnecessary deployments that are not utilized
-# delete_version() {
-#     local array=($@) #params
-#     for ((i=0; i< ${#array[@]}; ++i)); do
-#         if [[ ${array[$i]} == *"0.00"* ]]; then  #wildcard match any string containing 0.00
-#             local version=${array[$i-1]}
-#             local command="gcloud -q app versions delete $version"
-#             eval $command
-#         fi
-#     done        
-# }
+# FN Deletes unnecessary deployments that are not utilized
+delete_version() {
+    local array=($@) #params
+    for ((i=0; i< ${#array[@]}; ++i)); do
+        if [[ ${array[$i]} == *"0.00"* ]]; then  #wildcard match any string containing 0.00
+            local version=${array[$i-1]}
+            local command="gcloud -q app versions delete $version"
+            eval $command
+        fi
+    done        
+}
 
-# #execute fns
-# list=$(gcloud_list)
-# delete_version $list
+#execute fns
+list=$(gcloud_list)
+delete_version $list
 
 #deploy to google cloud provider
 gcloud -q app deploy --stop-previous-version 
